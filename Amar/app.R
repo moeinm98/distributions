319a847dc96254f7c6834d57e7e980374eb34b08
Amin <Amin@amins-MacBook-Pro-2.local>
2018-01-02 18:33:00 +0330

    gui completed/no estimation yet

--- Amar/app.R
+++ Amar/app.R
@@ -9,7 +9,7 @@ library("shinyFiles")
 
 ui <- navbarPage(
   theme = shinytheme("cerulean"),
-  "DISTs",
+  "Distributions",
   
   tabPanel("Uniform",
            
@@ -26,12 +26,12 @@ ui <- navbarPage(
                helpText("Tap 'Generate' button for generating a Uniform Random Number'"),
                
                actionButton("uniform_rand_gen_btn", "Generate", class = "btn-primary"),
-               tags$p("random generated number: ", textOutput("uniform_rand_num")),
+               tags$p("Random generated number: ", textOutput("uniform_rand_num")),
                
                
                
-               tags$h5("Get plot:"),
-               actionButton("uniform_plot_btn", "Plot", class = "btn-primary"),
+               tags$h5("Draw Plot:"),
+               actionButton("uniform_plot_btn", "Draw Plot", class = "btn-primary"),
                
                tags$h5("Choose the estimation file:"),
                helpText("with a estimation file that u upload I can estimate parameters of this distribution"),
@@ -59,17 +59,17 @@ ui <- navbarPage(
            sidebarLayout(
              sidebarPanel(
                helpText("Select parameter:"),
-               numericInput("bernouli_input", "Bernouli parameter :", value = 0.01,step = 0.1, min = 0, max = 1),
+               numericInput("bernouli_input", "Bernouli parameter :", value = 0.1,step = 0.1, min = 0, max = 1),
                tags$h5("Random number Generator:"),
                helpText("Tap 'Generate' button for generating a Bernouli Random Number'"),
                
                actionButton("bernouli_rand_gen_btn", "Generate", class = "btn-primary"),
-               tags$p("random generated number: ", textOutput("bernouli_rand_num")),
+               tags$p("Random generated number: ", textOutput("bernouli_rand_num")),
                
                
                
-               tags$h5("Get plot:"),
-               actionButton("bernouli_plot_btn", "Plot", class = "btn-primary"),
+               tags$h5("Draw Plot:"),
+               actionButton("bernouli_plot_btn", "Draw Plot", class = "btn-primary"),
                
                tags$h5("Choose the estimation file:"),
                helpText("with a estimation file that u upload I can estimate parameters of this distribution"),
@@ -96,18 +96,18 @@ ui <- navbarPage(
            sidebarLayout(
              sidebarPanel(
                helpText("A binomial random variable can be seen as the result of repeated Bernoulli Trials."),
-               numericInput("binomial_prob", "Binomial probability :", value = 0.01,step = 0.1, min = 0, max = 1),
+               numericInput("binomial_prob", "Binomial probability :", value = 0.1,step = 0.1, min = 0, max = 1),
                numericInput("binomial_n_trials", "Binomial number of trials", value = 1),
                tags$h5("Random number Generator:"),
                helpText("Tap 'Generate' button for generating a Binomial Random Number'"),
                
                actionButton("binomial_rand_gen_btn", "Generate", class = "btn-primary"),
-               tags$p("random generated number: ", textOutput("binomial_rand_num")),
+               tags$p("Random generated number: ", textOutput("binomial_rand_num")),
                
                
                
-               tags$h5("Get plot:"),
-               actionButton("binomial_plot_btn", "Plot", class = "btn-primary"),
+               tags$h5("Draw Plot:"),
+               actionButton("binomial_plot_btn", "Draw Plot", class = "btn-primary"),
                
                tags$h5("Choose the estimation file:"),
                helpText("with a estimation file that u upload I can estimate parameters of this distribution"),
@@ -134,17 +134,17 @@ ui <- navbarPage(
              sidebarPanel(
                helpText("The number of failures in Bernoulli trials, between two wins, follows the geometric distributions."),
                helpText("Select Geometric Distribution parameter:"),
-               numericInput("geometric_input", "Geometric parameter :", value = 0.01,step = 0.1, min = 0, max = 1),
+               numericInput("geometric_input", "Geometric parameter :", value = 0.1,step = 0.1, min = 0, max = 1),
                tags$h5("Random number Generator:"),
                helpText("Tap 'Generate' button for generating a Geometric Random Number'"),
                
                actionButton("geometric_rand_gen_btn", "Generate", class = "btn-primary"),
-               tags$p("random generated number: ", textOutput("geometric_rand_num")),
+               tags$p("Random generated number: ", textOutput("geometric_rand_num")),
                
                
                
-               tags$h5("Get plot:"),
-               actionButton("geometric_plot_btn", "Plot", class = "btn-primary"),
+               tags$h5("Draw Plot:"),
+               actionButton("geometric_plot_btn", "Draw Plot", class = "btn-primary"),
                
                tags$h5("Choose the estimation file:"),
                helpText("with a estimation file that u upload I can estimate parameters of this distribution"),
@@ -163,41 +163,155 @@ ui <- navbarPage(
            
   ),
   tabPanel("Exponential",
-             
-             
-             #Exponential - expgen TAB
-             
-             sidebarLayout(
-               sidebarPanel(
-                 helpText("Exponential distribution is a popular distribution which is used to model waiting times and memoryless processes.."),
-                 helpText("Select Exponential Distribution parameter:"),
-                 numericInput("exponential_input", "Exponential parameter :", value = 0.01, step = 0.1, min = 0.01),
-                 tags$h5("Random number Generator:"),
-                 helpText("Tap 'Generate' button for generating a Exponential Random Number'"),
-                 
-                 actionButton("exponential_rand_gen_btn", "Generate", class = "btn-primary"),
-                 tags$p("random generated number: ", textOutput("exponential_rand_num")),
-                 
-                 
-                 
-                 tags$h5("Get plot:"),
-                 actionButton("exponential_plot_btn", "Plot", class = "btn-primary"),
-                 
-                 tags$h5("Choose the estimation file:"),
-                 helpText("with a estimation file that u upload I can estimate parameters of this distribution"),
-                 shinyFilesButton("exponential_choose_file", "Choose a file" ,
-                                  title = "Please select a file:", multiple = FALSE,
-                                  buttonType = "default", class = "btn-primary"),
-                 
-                 textOutput("exponential_txt_file")   
-               ),
-               mainPanel(
-                 tags$h4("Exponential Plot:"),
-                 plotOutput("exponential_plot")
-                 
-               )
+           
+           
+           #Exponential - expgen TAB
+           
+           sidebarLayout(
+             sidebarPanel(
+               helpText("Exponential distribution is a popular distribution which is used to model waiting times and memoryless processes."),
+               helpText("Select Exponential Distribution parameter:"),
+               numericInput("exponential_input", "Exponential parameter(λ) :", value = 0.1, step = 0.1, min = 0.1),
+               tags$h5("Random number Generator:"),
+               helpText("Tap 'Generate' button for generating a Exponential Random Number'"),
+               
+               actionButton("exponential_rand_gen_btn", "Generate", class = "btn-primary"),
+               tags$p("Random generated number: ", textOutput("exponential_rand_num")),
+               
+               
+               
+               tags$h5("Draw Plot:"),
+               actionButton("exponential_plot_btn", "Draw Plot", class = "btn-primary"),
+               
+               tags$h5("Choose the estimation file:"),
+               helpText("with a estimation file that u upload I can estimate parameters of this distribution"),
+               shinyFilesButton("exponential_choose_file", "Choose a file" ,
+                                title = "Please select a file:", multiple = FALSE,
+                                buttonType = "default", class = "btn-primary"),
+               
+               textOutput("exponential_txt_file")   
+             ),
+             mainPanel(
+               tags$h4("Exponential Plot:"),
+               plotOutput("exponential_plot")
+               
              )
-             
+           )
+           
+  ),
+  tabPanel("Gamma",
+           
+           
+           #Gamma - gagen TAB
+           
+           sidebarLayout(
+             sidebarPanel(
+               helpText("Summation of k i.i.d exponential random variables leads to a gamma distributed random variable."),
+               helpText("Select Exponential Distribution parameter (λ) and k:"),
+               numericInput("gamma_input", "Gamma parameter :", value = 0.1, step = 0.1, min = 0.1),
+               numericInput("gamma_k", "k", value = 1, min = 1),
+               tags$h5("Random number Generator:"),
+               helpText("Tap 'Generate' button for generating a Gamma Random Number'"),
+               
+               actionButton("gamma_rand_gen_btn", "Generate", class = "btn-primary"),
+               tags$p("Random generated number: ", textOutput("gamma_rand_num")),
+               
+               
+               
+               tags$h5("Draw Plot:"),
+               actionButton("gamma_plot_btn", "Draw Plot", class = "btn-primary"),
+               
+               tags$h5("Choose the estimation file:"),
+               helpText("with a estimation file that you upload I can estimate parameters of this distribution"),
+               shinyFilesButton("gamma_choose_file", "Choose a file" ,
+                                title = "Please select a file:", multiple = FALSE,
+                                buttonType = "default", class = "btn-primary"),
+               
+               textOutput("gamma_txt_file")   
+             ),
+             mainPanel(
+               tags$h4("Gamma Plot:"),
+               plotOutput("gamma_plot")
+               
+             )
+           )
+           
+  ),
+  tabPanel("Poisson",
+           
+           
+           #Poisson - pogen TAB
+           
+           sidebarLayout(
+             sidebarPanel(
+               helpText("If an exponentially distributed variable is modeled as the waiting time before an arrival, the Poisson distributed variable can be modeled as the number of arrivals during a period of time of length t ."),
+               helpText("Select Poisson Distribution lambda and t:"),
+               numericInput("poisson_input", "Poisson parameter (λ) :", value = 0.1, step = 0.1, min = 0.1),
+               numericInput("poisson_t", "time (t)", value = 0.1, min = 0.1, step = 0.1),
+               tags$h5("Random number Generator:"),
+               helpText("Tap 'Generate' button for generating a Poisson Random Number'"),
+               
+               actionButton("poisson_rand_gen_btn", "Generate", class = "btn-primary"),
+               tags$p("Random generated number: ", textOutput("poisson_rand_num")),
+               
+               
+               
+               tags$h5("Draw Plot:"),
+               actionButton("poisson_plot_btn", "Draw Plot", class = "btn-primary"),
+               
+               tags$h5("Choose the estimation file:"),
+               helpText("with a estimation file that you upload I can estimate parameters of this distribution"),
+               shinyFilesButton("poisson_choose_file", "Choose a file" ,
+                                title = "Please select a file:", multiple = FALSE,
+                                buttonType = "default", class = "btn-primary"),
+               
+               textOutput("poisson_txt_file")   
+             ),
+             mainPanel(
+               tags$h4("Poisson Plot:"),
+               plotOutput("poisson_plot")
+               
+             )
+           )
+           
+  ),
+  tabPanel("Normal",
+           
+           
+           #Normal - nogen TAB
+           
+           sidebarLayout(
+             sidebarPanel(
+               helpText("The Poisson (λ) distribution can be considered as an approximation of N(λ,λ)"),
+               helpText("Select Normal Distribution mean (u) and variance (s):"),
+               numericInput("normal_u", "Normal parameter mean (u) :", value = 0.1, step = 0.1, min = 0),
+               numericInput("normal_s", "Normal parameter variance (s)", value = 0.1, step = 0.1, min = 0),
+               tags$h5("Random number Generator:"),
+               helpText("Tap 'Generate' button for generating a Normal Random Number'"),
+               
+               actionButton("normal_rand_gen_btn", "Generate", class = "btn-primary"),
+               tags$p("Random generated number: ", textOutput("normal_rand_num")),
+               
+               
+               
+               tags$h5("Draw Plot:"),
+               actionButton("normal_plot_btn", "Draw Plot", class = "btn-primary"),
+               
+               tags$h5("Choose the estimation file:"),
+               helpText("with a estimation file that you upload I can estimate parameters of this distribution"),
+               shinyFilesButton("normal_choose_file", "Choose a file" ,
+                                title = "Please select a file:", multiple = FALSE,
+                                buttonType = "default", class = "btn-primary"),
+               
+               textOutput("normal_txt_file")   
+             ),
+             mainPanel(
+               tags$h4("Normal Plot:"),
+               plotOutput("normal_plot")
+               
+             )
+           )
+           
   )
   
 )
@@ -244,7 +358,7 @@ server = function(input, output, session) {
   }
   cugen.visual <- function(){
     result=c()
-    for (i in 1:10000){
+    for (i in 1:1000){
       result=c(result,cugen())
     }
     qplot(result)
@@ -264,7 +378,7 @@ server = function(input, output, session) {
   brgen.visual <- function(p){
     library(ggplot2)
     result=c()
-    for (i in 1:10000){
+    for (i in 1:1000){
       result=c(result,brgen(p))
     }
     qplot(result)
@@ -356,7 +470,7 @@ server = function(input, output, session) {
   {
     library(ggplot2)
     c <- c()
-    for (i in 1:10000)
+    for (i in 1:1000)
       c[i] <- pogen(t, lambda)
     qplot(c)
   }
@@ -490,5 +604,73 @@ server = function(input, output, session) {
     }
   })
   
+  
+  
+  ## gagen - Gamma Tab
+  
+  
+  output$gamma_rand_num <- renderText({ input$gamma_rand_gen_btn
+    isolate(gagen(input$gamma_k,input$gamma_input)) })
+  
+  
+  output$gamma_plot <- renderPlot({input$gamma_plot_btn
+    isolate(gagen.visual(input$gamma_k,input$gamma_input)) })
+  
+  volumes = getVolumes()
+  observe({  
+    shinyFileChoose(input, "gamma_choose_file", roots = volumes, session = session)
+    
+    if(!is.null(input$gamma_choose_file)){
+      # browser()
+      file_selected<-parseFilePaths(volumes, input$gamma_choose_file)
+      output$gamma_txt_file <- renderText(as.character(file_selected$datapath))
+    }
+  })
+  
+  
+  ## pogen - Poisson Tab
+  
+  
+  output$poisson_rand_num <- renderText({ input$poisson_rand_gen_btn
+    isolate(pogen(input$poisson_t,input$poisson_input)) })
+  
+  
+  output$poisson_plot <- renderPlot({input$poisson_plot_btn
+    isolate(pogen.visual(input$poisson_t,input$poisson_input)) })
+  
+  volumes = getVolumes()
+  observe({  
+    shinyFileChoose(input, "poisson_choose_file", roots = volumes, session = session)
+    
+    if(!is.null(input$poisson_choose_file)){
+      # browser()
+      file_selected<-parseFilePaths(volumes, input$poisson_choose_file)
+      output$poisson_txt_file <- renderText(as.character(file_selected$datapath))
+    }
+  })
+  
+  
+  ## nogen - Normal Tab
+  
+  
+  output$normal_rand_num <- renderText({ input$normal_rand_gen_btn
+    isolate(nogen(input$normal_u,input$normal_s)) })
+  
+  
+  output$normal_plot <- renderPlot({input$normal_plot_btn
+    isolate(nogen.visual(input$normal_u,input$normal_s)) })
+  
+  volumes = getVolumes()
+  observe({  
+    shinyFileChoose(input, "normal_choose_file", roots = volumes, session = session)
+    
+    if(!is.null(input$normal_choose_file)){
+      # browser()
+      file_selected<-parseFilePaths(volumes, input$normal_choose_file)
+      output$normal_txt_file <- renderText(as.character(file_selected$datapath))
+    }
+  })
+  
 }
+#λ
 shinyApp(ui = ui, server = server)
