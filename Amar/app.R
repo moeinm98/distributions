4e2db34b16b8031c07b1a3b05377456e038d1bc2
Amin <Amin@amins-MacBook-Pro-2.local>
2018-01-02 17:38:04 +0330

    5 dists added

--- Amar/app.R
+++ Amar/app.R
@@ -59,7 +59,7 @@ ui <- navbarPage(
            sidebarLayout(
              sidebarPanel(
                helpText("Select parameter:"),
-               numericInput("bernouli_input", "Bernouli parameter :", value = 0),
+               numericInput("bernouli_input", "Bernouli parameter :", value = 0.01,step = 0.1, min = 0, max = 1),
                tags$h5("Random number Generator:"),
                helpText("Tap 'Generate' button for generating a Bernouli Random Number'"),
                
@@ -86,6 +86,118 @@ ui <- navbarPage(
              )
            )
            
+  ),
+  tabPanel("Binomial",
+           
+           
+           #Binomial - bigen TAB
+           
+           
+           sidebarLayout(
+             sidebarPanel(
+               helpText("A binomial random variable can be seen as the result of repeated Bernoulli Trials."),
+               numericInput("binomial_prob", "Binomial probability :", value = 0.01,step = 0.1, min = 0, max = 1),
+               numericInput("binomial_n_trials", "Binomial number of trials", value = 1),
+               tags$h5("Random number Generator:"),
+               helpText("Tap 'Generate' button for generating a Binomial Random Number'"),
+               
+               actionButton("binomial_rand_gen_btn", "Generate", class = "btn-primary"),
+               tags$p("random generated number: ", textOutput("binomial_rand_num")),
+               
+               
+               
+               tags$h5("Get plot:"),
+               actionButton("binomial_plot_btn", "Plot", class = "btn-primary"),
+               
+               tags$h5("Choose the estimation file:"),
+               helpText("with a estimation file that u upload I can estimate parameters of this distribution"),
+               shinyFilesButton("binomial_choose_file", "Choose a file" ,
+                                title = "Please select a file:", multiple = FALSE,
+                                buttonType = "default", class = "btn-primary"),
+               
+               textOutput("binomial_txt_file")   
+             ),
+             mainPanel(
+               tags$h4("Binomial Plot:"),
+               plotOutput("binomial_plot")
+               
+             )
+           )
+           
+  ),
+  tabPanel("Geometric",
+           
+           
+           #Geometric - gegen TAB
+           
+           sidebarLayout(
+             sidebarPanel(
+               helpText("The number of failures in Bernoulli trials, between two wins, follows the geometric distributions."),
+               helpText("Select Geometric Distribution parameter:"),
+               numericInput("geometric_input", "Geometric parameter :", value = 0.01,step = 0.1, min = 0, max = 1),
+               tags$h5("Random number Generator:"),
+               helpText("Tap 'Generate' button for generating a Geometric Random Number'"),
+               
+               actionButton("geometric_rand_gen_btn", "Generate", class = "btn-primary"),
+               tags$p("random generated number: ", textOutput("geometric_rand_num")),
+               
+               
+               
+               tags$h5("Get plot:"),
+               actionButton("geometric_plot_btn", "Plot", class = "btn-primary"),
+               
+               tags$h5("Choose the estimation file:"),
+               helpText("with a estimation file that u upload I can estimate parameters of this distribution"),
+               shinyFilesButton("geometric_choose_file", "Choose a file" ,
+                                title = "Please select a file:", multiple = FALSE,
+                                buttonType = "default", class = "btn-primary"),
+               
+               textOutput("geometric_txt_file")   
+             ),
+             mainPanel(
+               tags$h4("Geometric Plot:"),
+               plotOutput("geometric_plot")
+               
+             )
+           )
+           
+  ),
+  tabPanel("Exponential",
+             
+             
+             #Exponential - expgen TAB
+             
+             sidebarLayout(
+               sidebarPanel(
+                 helpText("Exponential distribution is a popular distribution which is used to model waiting times and memoryless processes.."),
+                 helpText("Select Exponential Distribution parameter:"),
+                 numericInput("exponential_input", "Exponential parameter :", value = 0.01, step = 0.1, min = 0.01),
+                 tags$h5("Random number Generator:"),
+                 helpText("Tap 'Generate' button for generating a Exponential Random Number'"),
+                 
+                 actionButton("exponential_rand_gen_btn", "Generate", class = "btn-primary"),
+                 tags$p("random generated number: ", textOutput("exponential_rand_num")),
+                 
+                 
+                 
+                 tags$h5("Get plot:"),
+                 actionButton("exponential_plot_btn", "Plot", class = "btn-primary"),
+                 
+                 tags$h5("Choose the estimation file:"),
+                 helpText("with a estimation file that u upload I can estimate parameters of this distribution"),
+                 shinyFilesButton("exponential_choose_file", "Choose a file" ,
+                                  title = "Please select a file:", multiple = FALSE,
+                                  buttonType = "default", class = "btn-primary"),
+                 
+                 textOutput("exponential_txt_file")   
+               ),
+               mainPanel(
+                 tags$h4("Exponential Plot:"),
+                 plotOutput("exponential_plot")
+                 
+               )
+             )
+             
   )
   
 )
@@ -304,10 +416,77 @@ server = function(input, output, session) {
   observe({  
     shinyFileChoose(input, "bernouli_choose_file", roots = volumes, session = session)
     
-    if(!is.null(input$uniform_choose_file)){
+    if(!is.null(input$bernouli_choose_file)){
       # browser()
       file_selected<-parseFilePaths(volumes, input$bernouli_choose_file)
       output$bernouli_txt_file <- renderText(as.character(file_selected$datapath))
+    }
+  })
+  
+  
+  ## binomial - Binomial Tab
+  
+  
+  output$binomial_rand_num <- renderText({ input$binomial_rand_gen_btn
+    isolate(bigen(input$binomial_prob, input$binomial_n_trials)) })
+  
+  
+  output$binomial_plot <- renderPlot({input$binomial_plot_btn
+    isolate(bigen.visual(input$binomial_prob, input$binomial_n_trials)) })
+  
+  volumes = getVolumes()
+  observe({  
+    shinyFileChoose(input, "binomial_choose_file", roots = volumes, session = session)
+    
+    if(!is.null(input$binomial_choose_file)){
+      # browser()
+      file_selected<-parseFilePaths(volumes, input$binomial_choose_file)
+      output$binomial_txt_file <- renderText(as.character(file_selected$datapath))
+    }
+  })
+  
+  
+  ## gegen - Geometric Tab
+  
+  
+  output$geometric_rand_num <- renderText({ input$geometric_rand_gen_btn
+    isolate(gegen(input$geometric_input)) })
+  
+  
+  output$geometric_plot <- renderPlot({input$geometric_plot_btn
+    isolate(gegen.visual(input$geometric_input)) })
+  
+  volumes = getVolumes()
+  observe({  
+    shinyFileChoose(input, "geometric_choose_file", roots = volumes, session = session)
+    
+    if(!is.null(input$geometric_choose_file)){
+      # browser()
+      file_selected<-parseFilePaths(volumes, input$geometric_choose_file)
+      output$geometric_txt_file <- renderText(as.character(file_selected$datapath))
+    }
+  })
+  
+  
+  
+  ## expgen - Exponential Tab
+  
+  
+  output$exponential_rand_num <- renderText({ input$exponential_rand_gen_btn
+    isolate(expgen(input$exponential_input)) })
+  
+  
+  output$exponential_plot <- renderPlot({input$exponential_plot_btn
+    isolate(expgen.visual(input$exponential_input)) })
+  
+  volumes = getVolumes()
+  observe({  
+    shinyFileChoose(input, "exponential_choose_file", roots = volumes, session = session)
+    
+    if(!is.null(input$exponential_choose_file)){
+      # browser()
+      file_selected<-parseFilePaths(volumes, input$exponential_choose_file)
+      output$exponential_txt_file <- renderText(as.character(file_selected$datapath))
     }
   })
   
